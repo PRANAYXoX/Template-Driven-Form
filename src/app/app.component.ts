64358dcc96df9cd6344c3156aa3c2b4cf3f62685
pranayXoX <pranay.dmnsns@gmail.com>
2023-04-10 01:42:09 +0530

    Basic Template Driven Form With Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { NgModel, NgForm, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'temp_driven_form';
+  name: String = '';
+  pass: String = '';
+  cPass: String = '';
+  addr: String = '';
+  onSubmit = (f: NgForm) => {
+    console.log('FORM OUTPUT:', f);
+  };
+  getVal = (f: any) => {
+    console.log('VAL::', f.name);
+    switch (f.name) {
+      case 'name':
+        console.log('NAME SELECTED');
+        break;
+      case 'pass':
+        console.log('Password selected');
+        break;
+      case 'c-pass':
+        console.log('C-Password selected');
+        break;
+      case 'addr':
+        console.log('Address selected');
+    }
+  };
 }
